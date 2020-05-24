52f1cc25162412eb498a7c1ff72670dcc9c608f3
null null <1719110459@utectulancingo.edu.mx>
2020-05-24 07:11:12 +0000

    banco.py concepto

---
+++ semana_2/banco.py
@@ -0,0 +1,23 @@
+class banco():
+    def __init__(self):
+        self.policias = 10
+        self.oficinas =  5
+        self.ventanillas =  15
+        self.efectivo = 15000
+        self.empleados = 152
+        self.sucursales = 150
+        self.valor_acciones = 15.7
+        self.intereses = 0
+        self.sede = 'guadalajara'
+        self.cuentas_activas = 2515
+    def deposito_de_efectivo():
+        print('deposito')
+    def retiro_de_efectivo():
+        print('retiro_de_efectivo')
+    def apertura_de_cuenta():
+        print('apertura_de_cuenta')
+    def cierre_de_cuenta():
+        print('cierre_de_cuenta')
+    def consulta_de_saldo():
+        print('consulta_de_saldo')
+hsbc = banco()
