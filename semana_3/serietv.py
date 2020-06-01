16791febd8a0657be4a205761384ee28c3da89be
Yael2mil <1719110459@utectulancingo.edu.mx>
2020-06-01 07:41:26 +0000

    serietv.py part1

---
+++ semana_3/serietv.py
@@ -0,0 +1,21 @@
+class serie_tv():
+    #atributos
+    plataforma = 'netflix'
+    resolucion = '3840 x 2160'
+    genero = ['drama', 'suspenso', 'romance']
+    productora = 'sony pictures'
+    idioma - 'español'
+    #metodo
+    def __init__(self):
+        pass
+    def reproducir(self):
+        print('reproducir')
+    def pausar(self):
+        print('pausar')
+
+class breaking_bad(serie_tv):
+    #atributos
+    nombre = 'Breaking bad'
+    temporadas = 5
+    capitulos = 64
+    
