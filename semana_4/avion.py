6f2dcfc8cffc06ac6997f74d131fcf2206013001
Yael2mil <1719110459@utectulancingo.edu.mx>
2020-06-05 22:01:26 +0000

    string.py terminado

---
+++ semana_4/avion.py
@@ -0,0 +1,29 @@
+class Avion():
+    #atributos
+    alas = 2
+    cabina = 1
+    pilotos = 2
+    alerones = 2
+    timon = 1
+    #metodos
+    def __init__(self):
+        pass
+    def ascender(self):
+        print('Ascender')
+    def descender(self):
+        print('Descender')
+
+class comercial(avion):
+    #atributos
+    pasajeros = 300
+    aerolinea = 'aeromexico'
+    aeromosas = 5
+    capacidad_tanque = "200000 litros"
+    capacidad_peso = "250 toneladas"
+    #metodos
+    def __init__(self):
+        pass
+    def despegue(self):
+        print('despegue')
+    def aterrizaje(self):
+        print('aterrizaje')
---
+++ semana_4/string.py
@@ -0,0 +1,16 @@
+while True:    #creamos el bucle while, para que el codigo se repita las veces necesarias
+    texto = input('introduzca la cadena a analizar: ')   #interactua con el programa para ingresar el texto a analizar
+    for caracter in texto: #el bucle for se encargara de leer el texto caracter por caracter
+        print(caracter) #imprime cada caracter para saber de cual se esta hablando
+        if caracter.isalpha():  #analiza si el caracter es una letra
+            print('Es una letra') #imprime el resultado
+        elif caracter.isnumeric(): #analiza si el caracter es un numero
+            print('Es un numero') #imprime el resultado
+        else:  #al no estar dentro de las anteriores damos por hecho que el caracter será un simbolo
+            print('Es un simbolo') #imprime el resultado
+    print(len(texto)) #imprime la longuitud de la cadena
+    sinespacios = texto.replace(" ", "") #elimina los espacios y los guarda en una nueva variable
+    print(len(sinespacios)) #imprime la longuitud de la cadena sin espacios
+    otra = input("¿Desea analizar otra cadena s/n?: ") #entrada que influye en el bucle while
+    if not (otra == "s" or otra == "S"): #condicion para analizar la entrada
+        break #rompe el bucle while
