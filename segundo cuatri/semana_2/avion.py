de66dd954742d8bd8e6e9dc1701804502211664f
Yael2mil <1719110459@utectulancingo.edu.mx>
2021-02-01 08:00:30 +0000

    hey

---
+++ segundo cuatri/semana_2/avion.py
@@ -0,0 +1,5 @@
+class Banco():
+    #automatizamos
+    def __init__(self):
+        #atributos
+        atributos = ['estado de cuenta', 'Monto de retiro', 'Nombre del cliente', 'Número de cuenta', 'Cuenta a la que se deposita''']
